6d426925ccbc3476c6eeb57ccbd8895761d1ceef
VicMg812 <vimg5943@gamil.com>
2024-03-03 23:29:13 -0400

    Mensaje de actualizacion

--- rest_client.py
+++ rest_client.py
@@ -22,7 +22,7 @@ eliminar_response = requests.request(method="GET"
                                 , url=ruta_eliminar)
 print(eliminar_response.text)
 
-# POST agrega un nuevo estudiante por la ruta /agrega_estudiante
+# Agregar estudiante carrera Economia
 ruta_post = url + "agrega_estudiante"
 nuevo_estudiante = {
     "nombre": "Juanito",
@@ -35,25 +35,7 @@ post_response = requests.request(method="POST",
                                 json=nuevo_estudiante)
 print(post_response.text)
 
-# GET consulta a la ruta /buscar_estudiante_id/{id}
-# ruta_filtrar_nombre = url + "buscar_estudiante_id/1"
-# filtrar_nombre_response = requests.request(method="GET", 
-#                                 url=ruta_filtrar_nombre)
-# print(filtrar_nombre_response.text)
 
-# POST actualiza un estudiante por la ruta /actualizar_estudiante
-# ruta_actualizar = url + "actualizar_estudiante"
-# estudiante_actualizado = {
-#     "id": 1,
-#     "nombre": "Juan",
-#     "apellido": "Pérez",
-#     "carrera": "Ingeniería Sistema",
-# }
-# actualizar_response = requests.request(
-#     method="POST", url=ruta_actualizar, 
-#     json=estudiante_actualizado
-# )
-# print(actualizar_response.text)
 #Agregar estudiantes con carrera de Economia
 ruta_post = url + "agrega_estudiante"
 nuevo_estudiante = {
@@ -66,25 +48,7 @@ post_response = requests.request(method="POST",
                                 url=ruta_post, 
                                 json=nuevo_estudiante)
 print(post_response.text)
-# ruta_post = url + "agrega_estudiante"
-# nuevo_estudiante = {
-#     "nombre": "Mariano",
-#     "apellido": "Garcia",
-#     "carrera": "Ingeniería Agronomica",
-# }
 
-# post_response = requests.request(method="POST", 
-#                                 url=ruta_post, 
-#                                 json=nuevo_estudiante)
-# print(post_response.text)
-#buscar estudiante con estudiante con nombre P
-# ruta_buscar_nombreP=url+"/buscar_estudiante_nombreP/"
-# buscar_nombreP_response=requests.request(method="GET",url=ruta_buscar_nombreP)
-# print(buscar_nombreP_response.text)
-#contar cuantas carreras hay
-# ruta_contar_carrera=url+"/contcarrera/"
-# contar_carrera_response=requests.request(method="GET",url=ruta_contar_carrera)
-# print(contar_carrera_response)
 #buscar estudiante con carrera Economia
 ruta_buscar_Economia=url+"/carreras/"
 buscar_Economia_response=requests.request(method="GET",url=ruta_buscar_Economia)
